3752a92af1fc22ab1f6933be7922858de8874c96
Dubinin Daniil <sacr1ficerq@gmail.com>
2025-07-02 17:22:51 +0300

    Add public test with gtest

--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -1,9 +1,24 @@
+#include <iostream>
+#include <print>
+#include <cstdint>
+#include <vector>
+
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include "orderbook/utils.hpp"
+
+TEST(PublicTest, result) {
+    orderbook::UpdateRow row1 = {{100, 5}, {110, 10}, {111, 1}};
+    orderbook::UpdateRow row2 = {{100, 1}};
+    orderbook::UpdateRow row3 = {{110, 0}, {120, 3}};
+
+    std::vector<orderbook::UpdateRowWithShares> updates = {
+        {row1, 15},
+        {row2, 12},
+        {row3, 4}
+    };
+
+    uint64_t result = orderbook::Solve(updates);
+
+    EXPECT_EQ(result, 668);
 }
